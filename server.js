23f75176fdbe7e0d07355f26c6005633ae493db9
Luvlaeee <lovelyoren0@gmail.com>
2026-04-13 15:13:44 +0800

    fix: add dotenv and correct model name

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ require('dotenv').config();  // ← ADD THIS at the top
 const express = require('express');
 const cors = require('cors');
 // ... rest of your code
-
-const express = require('express');
-const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
